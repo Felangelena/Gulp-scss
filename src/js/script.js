1bf6043080cdc035aed4444ae2e41aff96eadbac
Olena Oliianenko <felangelena@gmail.com>
2023-06-17 22:53:19 +0200

    feat: add section hero

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('nav--active');
 });
 
-/* menuItem.forEach(item => {
-    item.addEventListener('click', () => {
-        menu.classList.toggle('active');
-    })
-}); */
+nav.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('nav--active');
+})
