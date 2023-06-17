30da840b0cb266e1214f024d6e65a10e80c98688
Olena Oliianenko <felangelena@gmail.com>
2023-06-17 22:15:09 +0200

    feat: add hamburger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
 
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger--active');
+});
+
+/* menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    })
+}); */
